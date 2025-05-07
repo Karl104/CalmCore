b4abeff5fc21064830228eb4aabddff19d5bf7cc
Karl <karllastimoso454@gmail.com>
2025-05-07 13:06:27 +0800

    feat(router): add new routes for library, reflection, history, register, login, and forget-password

--- FrontEnd/src/router/index.js
+++ FrontEnd/src/router/index.js
@@ -13,6 +13,39 @@ const router = createRouter({
       name: 'inspiration',
       component: () => import('../views/InspirationView.vue'),
     },
+    {
+      path: '/library',
+      name: 'library',
+      component: () => import('../views/LibraryView.vue')
+    },
+    {
+      path: '/reflection',
+      name: 'reflection',
+      component: () => import('../views/ReflectionView.vue')
+    },
+    {
+      path: '/history',
+      name: 'history',
+      component: () => import('../views/HistoryView.vue')
+    },
+    {
+      path: '/register',
+      name: 'register',
+      // Use dynamic import consistent with other routes
+      component: () => import('../views/RegisterView.vue')
+    },
+
+    {
+      path: '/login',
+      name: 'login',
+
+      component: () => import('../views/LoginView.vue')
+    },
+    {
+      path: '/forget',
+      name: 'forget-password',
+      component: () => import('../views/ForgetPass.vue')
+    }
   ],
 })
 
